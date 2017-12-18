b6eff909d9f6a01c00e1e453dae37254fe9e4027
ferraith <ferraith@gmail.com>
2017-12-18 15:30:47 +0100

    Integrated django-money app into stockpot project

--- stockpot/settings.py
+++ stockpot/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.apps.PortfolioConfig'
+    'djmoney',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
